f51227f6773c447bdb37809a3f517bf075d4f4b3
D. Todd <dt@network.net>
2022-10-29 11:27:29 -0700

    Fixed timing routine to use high_resolution_clock. Confirmed timing was as expected in terms of real time. Noted that using a number of threads that exceeded machine capacity, did not slow result, since C++ sleep routine effieciently switches to another thread.

--- parallel_processing/multi_thread.cpp
+++ parallel_processing/multi_thread.cpp
@@ -1,36 +1,52 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <ratio>
 #include <thread>
 #include <time.h>
 
-std::chrono::milliseconds timespan(1500); // or whatever
+using namespace std::chrono;
 
-void beauty() {
+milliseconds TIME_SPAN(5000); // 5 seconds (or whatever)
+
+
+// Have the threads actually do something that really takes time,
+// to see how many CPUs are put to work.
+
+void beauty(milliseconds time_to_sleep) {
     std::cout << "\nwaiting..." << std::endl;
-    std::this_thread::sleep_for(timespan);
-    std::cout << "\nBeauty is only skin deep\n.\n.\n.\n" << std::endl;
+    std::this_thread::sleep_for(time_to_sleep);
+    std::cout << "\nBeauty is only skin deep\n.\n.\n." << std::endl;
 }
 
 int test_multithreading() {
     clock_t start, end;
     double elapsed;
-    start = clock();
-    std::thread t1(beauty);
-    std::thread t2(beauty);
+    std::thread t2;
+    
+    high_resolution_clock::time_point start_t = high_resolution_clock::now();
+    
+    std::thread t1(beauty, TIME_SPAN);
+    t2 = std::thread(beauty, TIME_SPAN);  // alternative form.
+    std::thread t3(beauty, TIME_SPAN);
+    std::thread t4(beauty, TIME_SPAN);
+    std::thread t5(beauty, TIME_SPAN);
     t1.join();
-    //std::thread t2(beauty);
     t2.join();
-    end = clock();
-    elapsed = ((double) (end - start))/((double) 1000);
-    std::cout << "Elapsed ticks for serial execution (in thousands): " << elapsed << "\n" << std::endl;
+    t3.join();
+    t4.join();
+    t5.join();
+
+    high_resolution_clock::time_point end_t = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(end_t - start_t);
+
+    std::cout << "\nElapsed time in seconds: " << time_span.count() << "\n" << std::endl;
 
     return 0;
 }
 
-
-
 int main() {
+    int parallel = 1;
     return test_multithreading();
 
 }
